d6031f0962eb082fb6b5a6aeed1adf16c4e41d55
Andrei Podvornyi <44447614+andrewpodvorniy@users.noreply.github.com>
2022-04-19 20:41:16 +0300

    Added base list component (#2)

---
+++ src/components/base/BaseList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="list">
+    <BaseListItem v-for="item in items" :key="item.id" :title="item.title" />
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BaseList',
+};
+</script>
+
+<script setup lang="ts">
+import type { ListItem } from '@/types/list-item.type';
+import BaseListItem from '@/components/base/BaseListItem.vue';
+
+defineProps<{
+  items: ListItem[];
+}>();
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/styles/extends/list.extends';
+
+.list {
+  @extend #list;
+}
+</style>
