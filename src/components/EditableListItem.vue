bf4bc84060fb962f23b8027663ce86a1cc7b5cfa
Andrei Podvornyi <44447614+andrewpodvorniy@users.noreply.github.com>
2022-04-20 00:36:15 +0300

    Added editable list (#4)

---
+++ src/components/EditableListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <li class="list-item">
+    <BaseTextField
+      placeholder="Enter list item title..."
+      :model-value="inputValue"
+      @update:model-value="onInput"
+    />
+  </li>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import debounce from 'just-debounce';
+import type { ListItem } from '@/types/list-item.type';
+import BaseTextField from '@/components/base/BaseTextField.vue';
+
+const props = defineProps<{
+  item: ListItem;
+}>();
+
+const emit = defineEmits<{
+  (e: 'updateItem', item: ListItem): void;
+}>();
+
+const inputValue = ref(props.item.title);
+
+const onInput = debounce((value: string) => {
+  inputValue.value = value;
+
+  const updatedItem = {
+    id: props.item.id,
+    title: value,
+  };
+
+  emit('updateItem', updatedItem);
+}, 300);
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/styles/extends/list.extends';
+
+.list-item {
+  @extend #list-item;
+}
+</style>
---
+++ src/components/base/BaseTextField.vue
@@ -0,0 +1,49 @@
+<template>
+  <input
+    type="text"
+    class="text-field"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="handleInput"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modelValue: string;
+  placeholder?: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void;
+}>();
+
+const handleInput = (event: Event) =>
+  emit('update:modelValue', (event.target as HTMLInputElement).value);
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/functions';
+@import '@/assets/styles/variables';
+
+.text-field {
+  width: 100%;
+  height: toRem(46);
+  padding: toRem(10) toRem(14);
+  color: $grey-5;
+  background-color: $white-1;
+  border-radius: $default-border-radius;
+  border: 1px solid $grey-3;
+  font-size: toRem(16);
+  font-family: inherit;
+  box-shadow: 0 1px 1px 1px $grey-1;
+  transition: color $hover-duration, background-color $hover-duration,
+    border-color $hover-duration, box-shadow $hover-duration;
+
+  &:focus {
+    border-color: $blue-1;
+    box-shadow: 0 0 0 3px $blue-2;
+    outline: none;
+  }
+}
+</style>
