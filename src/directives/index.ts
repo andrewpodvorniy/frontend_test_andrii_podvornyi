79eab56cfd05e51c4fc248b3b880d23fbab6c82a
Andrei Podvornyi <44447614+andrewpodvorniy@users.noreply.github.com>
2022-04-20 17:30:53 +0300

    Updated v-trigger-enter directory (#15)

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,4 +1,4 @@
-import VEnterClick from '@/directives/v-trigger-enter';
+import VTriggerEnter from '@/directives/v-trigger-enter';
 import VFocus from '@/directives/v-focus';
 
-export default [VEnterClick, VFocus];
+export default [VTriggerEnter, VFocus];
