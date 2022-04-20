47eba657d66b0b20203684cc1e2f0e2dc49abbd6
Andrew Podvornyi <andrewpodvorniy@gmail.com>
2022-04-20 17:26:21 +0300

    Removed default items

--- src/services/list-items.service.ts
+++ src/services/list-items.service.ts
@@ -2,12 +2,6 @@ import type { ListItem } from '@/types/list-item.type';
 import { SAVED_ITEMS_LOCAL_STORAGE_KEY } from '@/constants/local-storage.constants';
 import type { ILocalStorageService } from '@/types/local-storage-service.type';
 
-const initialList = [
-  { id: '1', title: 'Item 1' },
-  { id: '2', title: 'Item 2' },
-  { id: '3', title: 'Item 3' },
-];
-
 class ListItemsService {
   constructor(private _localStorageService: ILocalStorageService) {}
 
@@ -16,7 +10,7 @@ class ListItemsService {
       SAVED_ITEMS_LOCAL_STORAGE_KEY
     );
 
-    return data ?? initialList;
+    return data ?? [];
   }
 
   saveItems(data: ListItem[]) {
